488a6895cde87a264dfaf2aeebb59c87a3fde6f4
razi <rezaie127@yahoo.com>
2022-03-15 00:49:55 +0330

    add fragments

---
+++ app/src/main/java/com/example/hw10_3/SettingFragment.kt
@@ -0,0 +1,32 @@
+package com.example.hw10_3
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.hw10_3.databinding.FragmentHomeBinding
+import com.example.hw10_3.databinding.FragmentSettingBinding
+
+class SettingFragment : Fragment() {
+    lateinit var binding: FragmentSettingBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentSettingBinding.inflate (inflater, container, false)
+        return binding.root
+        // Inflate the layout for this fragment
+        //return inflater.inflate(R.layout.fragment_setting, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
