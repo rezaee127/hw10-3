ed4e6f1796f2f0125e39806f9892adb2d60231ba
razi <rezaie127@yahoo.com>
2022-03-29 02:15:52 +0430

    edit Storage & fragments

--- app/src/main/java/com/example/hw10_3/MainActivity.kt
+++ app/src/main/java/com/example/hw10_3/MainActivity.kt
@@ -1,22 +1,16 @@
 package com.example.hw10_3
 
 import android.content.Context
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
-import android.view.MenuInflater
-import android.view.View
-import android.widget.Toast
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.example.hw10_3.Storage.arrayOfItems
 import com.example.hw10_3.databinding.ActivityMainBinding
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import android.view.MenuItem as MenuItem1
 
 class MainActivity : AppCompatActivity() {
